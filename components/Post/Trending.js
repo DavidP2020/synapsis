50b61dda83cdb60c8a1c46f7936b93315df4fc43
DavidP2020 <David11Pangestu@gmail.com>
2023-02-24 22:01:30 +0700

    fix_bugs_trending

--- components/Post/Trending.js
+++ components/Post/Trending.js
@@ -145,9 +145,9 @@ function Topic() {
                   <Fade in={open}>
                     <Box
                       sx={style}
-                      style={{ background: "white" }}
+                      style={{ background: "white", width: "80%" }}
                       component={"div"}
-                      className="w-3/4 overflow-auto max-h-full"
+                      className="overflow-auto max-h-full"
                     >
                       <Toolbar style={{ marginLeft: "-1rem" }}>
                         <Typography component="div" sx={{ flexGrow: 2 }}>
